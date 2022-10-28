e3324f0c3570f21a80d76ce0ff11d1fba527a085
toty11 <xtoty971@gmail.com>
2022-10-28 17:19:54 -0400

    main

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import logo from '../logo.svg';
+import './App.css';
+import Menu from './Menu'
+
+function App() {
+  return (
+    <Menu/>
+  );
+}
+
+export default App;
---
+++ src/components/Menu.js
@@ -0,0 +1,20 @@
+import '../styles/menu.css'
+
+function Menu() {
+    return <section>
+        <h1>Menu</h1>
+        <ul>
+            <li>
+                <a href="#">Produits</a>
+            </li>
+            <li>
+                <a href="#">Coaching</a>
+            </li>
+            <li>
+                <a href="#">Rendez-vous</a>
+            </li>
+        </ul>
+    </section>
+}
+
+export default Menu
